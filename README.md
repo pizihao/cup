146f9980c229f130f62bae7fc6a956967ce5c13f
liuwenh <2335715300@qq.com>
2020-12-21 16:14:43 +0800

    底层搭建

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cup, 一个ORM框架
+
+![cup](cup.png)
+
+### GOOD LUCK
---
+++ src/main/java/com/qlu/cup/type/JdbcType.java
@@ -0,0 +1,11 @@
+package com.qlu.cup.type;
+
+/**
+ * @program: cup
+ * @description: jdbc数据类型枚举
+ * @author: liuwenhao
+ * @create: 2020-12-21 16:13
+ **/
+public enum JdbcType {
+
+}
